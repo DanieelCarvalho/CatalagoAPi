93709282a69f83bbb5a22a2e95e968ab885ed7ab
Daniel <danielscg2012@gmail.com>
2026-03-01 11:03:59 -0300

    first commit

---
+++ CatalogoApi/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿namespace CatalogoApi.Models;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? Descrição { get; set; }
+
+    public decimal? Preco { get; set; }
+
+    public string? ImagemUrl { get; set; }
+
+    public float Estoque { get; set; }
+
+    public DateTime? DataCadastro { get; set; }
+}
